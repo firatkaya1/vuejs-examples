29877fe723d734f6381bc410a90c343185b3fa9d
firatkaya1 <firat.kaya@mail.com>
2021-02-17 18:25:56 +0300

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() {
+    return {
+      todos:[
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Two",
+          completed:true
+        },
+        {
+          id:3,
+          title:"Todo Three",
+          completed:false
+        },
+        {
+          id:4,
+          title:"Todo Four",
+          completed:true
+        }
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos,newTodo];
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+  width: 50%;
+  margin:20px auto;
+  }
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+.btn:focus,
+input[type="text"]:focus {
+  outline: 0;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="header">
+        <h1>Todo</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+.header a {
+    color: #fff;
+    padding-right: 5px;
+}
+</style>
